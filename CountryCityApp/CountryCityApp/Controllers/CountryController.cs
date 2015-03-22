5dd058aacb1fed6f144033fad6d15f783d1fd07d
Mukit Ul Hasan <hasan.mukit7@gmail.com>
2015-03-22 10:53:05 +0600

    Friday's work
    still to do a lot of changes

---
+++ CountryCityApp/CountryCityApp/Controllers/CountryController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CountryCityApp.Models;
+
+namespace CountryCityApp.Controllers
+{
+    public class CountryController : Controller
+    {
+        CountryGateway aCountryGateway=new CountryGateway();
+        //
+        // GET: /Country/
+        public ActionResult Save()
+        {
+            ViewBag.countries = aCountryGateway.GetAllCountry().OrderBy(x => x.Name).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Save(Country aCountry, HttpPostedFileBase picture)
+        {
+            try
+            {
+                string fileName = Path.GetFileName(picture.FileName);
+                string path = Path.Combine(Server.MapPath("~/Pictures"), fileName);
+                picture.SaveAs(path);
+                aCountry.Picture = "/Pictures/" + fileName;
+            }
+            catch
+            {
+                
+            }
+            aCountryGateway.Save(aCountry);
+            ViewBag.countries = aCountryGateway.GetAllCountry().OrderBy(x => x.Name).ToList();
+            return View();
+
+        }
+
+        public JsonResult CheckName(string name)
+        {
+            Country aCountry = aCountryGateway.GetAllCountry().FirstOrDefault(x => x.Name == name);
+            if (aCountry == null)
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+            return Json(false, JsonRequestBehavior.AllowGet);
+        }
+	}
+}
---
+++ CountryCityApp/CountryCityApp/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CountryCityApp.Models
+{
+    public class Country
+    {
+        [Key]
+        public int Id { set; get; }
+        [Required(ErrorMessage = "Name is required")]
+        [Remote("CheckName","Country",ErrorMessage = "This name is already exists")]
+        public string Name { set; get; }
+        [Required(ErrorMessage = "About is required")]
+        [AllowHtml]
+        public string About { set; get; }
+        [Required(ErrorMessage = "Picture is required")]
+        public string Picture { set; get; }
+    }
+}
